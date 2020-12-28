8ee788ff1d2d0e4127f0b5c4280df435be4f62f0
Tehman Sardar <tehmansardar@gmail.com>
2020-12-29 02:11:49 +0500

    Feed Form More Icon

--- front/src/components/MessageSender.js
+++ front/src/components/MessageSender.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Avatar, Input } from '@material-ui/core';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import './css/MessageSender.css';
 const MessageSender = () => {
 	const [input, setInput] = useState('');
@@ -32,6 +35,20 @@ const MessageSender = () => {
 					/>
 					<button type='submit'>Submit</button>
 				</form>
+			</div>
+			<div className='messageSender__bottom'>
+				<div className='messageSender__option'>
+					<VideocamIcon style={{ color: 'red' }} />
+					<h3>Live Video</h3>
+				</div>
+				<div className='messageSender__option'>
+					<PhotoLibraryIcon style={{ color: 'green' }} />
+					<h3>Photo/Video</h3>
+				</div>
+				<div className='messageSender__option'>
+					<InsertEmoticonIcon style={{ color: 'orange' }} />
+					<h3>Feeling/Activity</h3>
+				</div>
 			</div>
 		</div>
 	);
